8dd3a21e68109f911b386d7eb5ee96a0653814ee
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-02-23 17:11:13 -0500

    FIX: Find packages correctly

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 import sys
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
+
+SUBPACKAGE = "fsl"
 
 # Give setuptools a hint to complain if it's too old a version
 # 30.3.0 allows us to put most metadata in setup.cfg
 # Should match pyproject.toml
-SETUP_REQUIRES = ['setuptools >= 30.3.0']
+SETUP_REQUIRES = ["setuptools >= 30.3.0"]
 # This enables setuptools to install wheel on-the-fly
-SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
+SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
 if __name__ == "__main__":
-    setup(name='pydra-fsl',
-          setup_requires=SETUP_REQUIRES,
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass())
+    setup(
+        name=f"pydra-{SUBPACKAGE}",
+        setup_requires=SETUP_REQUIRES,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        packages=[
+            f"pydra.tasks.{SUBPACKAGE}.{package}"
+            for package in find_packages("pydra/tasks/{SUBPACKAGE}")
+        ],
+    )
