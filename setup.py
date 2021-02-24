6d5e1c9689bf0a0a21ee23a9e2e0d92a3d55d60d
Christopher J. Markiewicz <markiewicz@stanford.edu>
2021-02-24 13:02:25 -0500

    MNT: Drop tests_require, since setup.py test is deprecated and we misspelled it anyway

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ SUBPACKAGE = "TODO"
 
 # Give setuptools a hint to complain if it's too old a version
 # 30.3.0 allows us to put most metadata in setup.cfg
+# 40.1.0 enables the `find_namespace:` directive
 # Should match pyproject.toml
-SETUP_REQUIRES = ["setuptools >= 30.3.0"]
+SETUP_REQUIRES = ["setuptools >= 40.1.0"]
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
 
