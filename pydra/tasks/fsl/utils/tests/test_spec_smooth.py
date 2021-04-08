3a303f9ad46f8bb226227aa944d8a33f7bef6fd7
Hao-Ting Wang <htwangtw@gmail.com>
2021-04-08 17:27:46 +0100

    NF interface utils.smooth

---
+++ pydra/tasks/fsl/utils/tests/test_spec_smooth.py
@@ -0,0 +1,19 @@
+import os, pytest
+from pathlib import Path
+from ..smooth import Smooth
+
+
+@pytest.mark.parametrize("inputs, outputs", [({"sigma": 3.397}, ["smoothed_file"])])
+def test_Smooth(test_data, inputs, outputs):
+    in_file = Path(test_data) / "test.nii.gz"
+    if inputs is None:
+        inputs = {}
+    for key, val in inputs.items():
+        try:
+            inputs[key] = eval(val)
+        except:
+            pass
+    task = Smooth(in_file=in_file, **inputs)
+    assert set(task.generated_output_names) == set(
+        ["return_code", "stdout", "stderr"] + outputs
+    )
