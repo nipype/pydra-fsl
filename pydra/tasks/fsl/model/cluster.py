722a1691cc0faf6e221987fbf6741af8345fe458
yibeichan <yibeichen@ucsb.edu>
2022-07-26 16:01:46 -0700

    fix conflicts

--- pydra/tasks/fsl/model/cluster.py
+++ pydra/tasks/fsl/model/cluster.py
@@ -229,7 +229,6 @@ class Cluster(ShellCommandTask):
     >>> task.inputs.use_mm = True
     >>> task.cmdline # doctest: +SKIP
     'cluster --in=zstat1.nii.gz --thresh=2.3000000000 --mm'
-
     """
 
     input_spec = Cluster_input_spec
