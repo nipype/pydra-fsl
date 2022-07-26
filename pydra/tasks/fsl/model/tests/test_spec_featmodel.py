7dd63f3f02b117a5a524d1535450894753e6fca1
yibeichan <yibeichen@ucsb.edu>
2022-07-25 20:52:51 -0700

    all tests passed locally

---
+++ pydra/tasks/fsl/model/tests/test_spec_featmodel.py
@@ -0,0 +1,66 @@
+import re, os, shutil, pytest
+from pathlib import Path
+from ..featmodel import FEATModel
+
+
+@pytest.mark.parametrize("inputs, outputs", [])
+def test_FEATModel(test_data, inputs, outputs):
+    if inputs is None:
+        in_file = Path(test_data) / "test.nii.gz"
+        task = FEATModel(in_file=in_file)
+    else:
+        for key, val in inputs.items():
+            try:
+                pattern = r"\.[a-zA-Z]*"
+                if isinstance(val, str):
+                    if re.findall(pattern, val) != []:
+                        inputs[key] = Path(test_data) / val
+                    elif "_dir" in key:
+                        dirpath = Path(test_data) / val
+                        if dirpath.exists() and dirpath.is_dir():
+                            shutil.rmtree(dirpath)
+                        inputs[key] = Path(test_data) / val
+                    else:
+                        inputs[key] = eval(val)
+                elif isinstance(val, list):
+                    if all(re.findall(pattern, _) != [] for _ in val):
+                        inputs[key] = [Path(test_data) / _ for _ in val]
+                else:
+                    inputs[key] = eval(val)
+            except:
+                pass
+        task = FEATModel(**inputs)
+    assert set(task.generated_output_names) == set(
+        ["return_code", "stdout", "stderr"] + outputs
+    )
+
+
+@pytest.mark.parametrize("inputs, error", [(None, "AttributeError")])
+def test_FEATModel_exception(test_data, inputs, error):
+    if inputs is None:
+        in_file = Path(test_data) / "test.nii.gz"
+        task = FEATModel(in_file=in_file)
+    else:
+        for key, val in inputs.items():
+            try:
+                pattern = r"\.[a-zA-Z]*"
+                if isinstance(val, str):
+                    if re.findall(pattern, val) != []:
+                        inputs[key] = Path(test_data) / val
+                    elif "_dir" in key:
+                        dirpath = Path(test_data) / val
+                        if dirpath.exists() and dirpath.is_dir():
+                            shutil.rmtree(dirpath)
+                        inputs[key] = Path(test_data) / val
+                    else:
+                        inputs[key] = eval(val)
+                elif isinstance(val, list):
+                    if all(re.findall(pattern, _) != [] for _ in val):
+                        inputs[key] = [Path(test_data) / _ for _ in val]
+                else:
+                    inputs[key] = eval(val)
+            except:
+                pass
+        task = FEATModel(**inputs)
+    with pytest.raises(eval(error)):
+        task.generated_output_names
