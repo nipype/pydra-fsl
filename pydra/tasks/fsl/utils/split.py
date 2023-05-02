c250501971b7dd7ff2c44836cf3efe425be04e05
yibeichan <yibeichen@ucsb.edu>
2023-05-02 10:31:18 -0700

    redo split

--- pydra/tasks/fsl/utils/split.py
+++ pydra/tasks/fsl/utils/split.py
@@ -2,6 +2,14 @@ from pydra.engine import specs
 from pydra import ShellCommandTask
 import typing as ty
 
+
+def Split_output(inputs):
+    import os, glob
+
+    output_dir = os.getcwd()
+    return sorted(glob.glob(output_dir, f"{inputs.output_basename}*.*"))
+
+
 input_fields = [
     (
         "in_file",
@@ -29,15 +37,37 @@ input_fields = [
         },
     ),
 ]
-Split_input_spec = specs.SpecInfo(name="Input", fields=input_fields, bases=(specs.ShellSpec,))
+Split_input_spec = specs.SpecInfo(
+    name="Input", fields=input_fields, bases=(specs.ShellSpec,)
+)
 
-output_fields = []
+output_fields = [
+    (
+        "out_files",
+        specs.MultiOutputFile,
+        {
+            "requires": ["in_file", "output_basename", "dimension"],
+            "callable": "Split_output",
+        },
+    )
+]
 Split_output_spec = specs.SpecInfo(
     name="Output", fields=output_fields, bases=(specs.ShellOutSpec,)
 )
 
 
 class Split(ShellCommandTask):
+    """
+    Example
+    -------
+    >>> task = Split()
+    >>> task.inputs.in_file = "test.nii.gz"
+    >>> task.inputs.output_basename = "test_split"
+    >>> task.inputs.dimension = "t"
+    >>> task.cmdline
+    'fslsplit test.nii.gz test_split -t'
+    """
+
     input_spec = Split_input_spec
     output_spec = Split_output_spec
     executable = "fslsplit"
--- pydra/tasks/fsl/utils/tests/test_spec_split.py
+++ pydra/tasks/fsl/utils/tests/test_spec_split.py
@@ -3,7 +3,19 @@ from pathlib import Path
 from ..split import Split
 
 
-@pytest.mark.parametrize("inputs, outputs", [])
+@pytest.mark.parametrize(
+    "inputs, outputs",
+    [
+        (
+            {
+                "in_file": "test.nii.gz",
+                "output_basename": "test_split",
+                "dimension": "t",
+            },
+            ["out_files"],
+        )
+    ],
+)
 def test_Split(test_data, inputs, outputs):
     if inputs is None:
         in_file = Path(test_data) / "test.nii.gz"
@@ -30,35 +42,6 @@ def test_Split(test_data, inputs, outputs):
             except:
                 pass
         task = Split(**inputs)
-    assert set(task.generated_output_names) == set(["return_code", "stdout", "stderr"] + outputs)
-
-
-@pytest.mark.parametrize("inputs, error", [(None, "AttributeError")])
-def test_Split_exception(test_data, inputs, error):
-    if inputs is None:
-        in_file = Path(test_data) / "test.nii.gz"
-        task = Split(in_file=in_file)
-    else:
-        for key, val in inputs.items():
-            try:
-                pattern = r"\.[a-zA-Z]*"
-                if isinstance(val, str):
-                    if re.findall(pattern, val) != []:
-                        inputs[key] = Path(test_data) / val
-                    elif "_dir" in key:
-                        dirpath = Path(test_data) / val
-                        if dirpath.exists() and dirpath.is_dir():
-                            shutil.rmtree(dirpath)
-                        inputs[key] = Path(test_data) / val
-                    else:
-                        inputs[key] = eval(val)
-                elif isinstance(val, list):
-                    if all(re.findall(pattern, _) != [] for _ in val):
-                        inputs[key] = [Path(test_data) / _ for _ in val]
-                else:
-                    inputs[key] = eval(val)
-            except:
-                pass
-        task = Split(**inputs)
-    with pytest.raises(eval(error)):
-        task.generated_output_names
+    assert set(task.generated_output_names) == set(
+        ["return_code", "stdout", "stderr"] + outputs
+    )
