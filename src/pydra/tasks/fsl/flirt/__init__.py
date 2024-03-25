844d9b809bd3a094c9395f2ba040d67da8d0e019
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:47:07 +1100

    REF: Move all convert_xfm based utils to same module
    Also adds missing FixScaleSkew task in case someone needs it.

--- src/pydra/tasks/fsl/flirt/__init__.py
+++ src/pydra/tasks/fsl/flirt/__init__.py
@@ -1,7 +1,5 @@
-from .concatxfm import ConcatXFM
-from .convertxfm import ConvertXFM
+from .convertxfm import ConcatXFM, ConvertXFM, FixScaleSkew, InvertXFM
 from .flirt import FLIRT, ApplyXFM
 from .img2imgcoord import Img2ImgCoord
 from .img2stdcoord import Img2StdCoord
-from .invertxfm import InvertXFM
 from .std2imgcoord import Std2ImgCoord
--- src/pydra/tasks/fsl/flirt/convertxfm.py
+++ src/pydra/tasks/fsl/flirt/convertxfm.py
@@ -5,20 +5,32 @@ ConvertXFM
 Examples
 --------
 
->>> task = ConvertXFM(input_matrix="input.mat", inverse=True)
->>> task.cmdline  # doctest: +ELLIPSIS
-'convert_xfm -omat ...input_cxfm.mat -inverse input.mat'
+Concatenate transformation matrix:
 
->>> task = ConvertXFM(
-...     input_matrix="AtoB.mat",
-...     concat_matrix="BtoC.mat",
-...     output_matrix="AtoC.mat",
-... )
+>>> task = ConcatXFM(input_matrix="AtoB.mat", concat_matrix="BtoC.mat", output_matrix="AtoC.mat")
 >>> task.cmdline
 'convert_xfm -omat AtoC.mat -concat BtoC.mat AtoB.mat'
+
+Invert transformation matrix:
+
+>>> task = InvertXFM(input_matrix="AtoB.mat", output_matrix="BtoA.mat")
+>>> task.cmdline
+'convert_xfm -omat BtoA.mat -inverse AtoB.mat'
+
+Fix scaling and skewness with additional matrix:
+
+>>> task = FixScaleSkew(input_matrix="A.mat", fixscaleskew_matrix="B.mat")
+>>> task.cmdline
+'convert_xfm -omat ...A_cxfm.mat -fixscaleskew B.mat A.mat'
+
+Use ConvertXFM to combine multiple operations at once, such as concatenation and inversion:
+
+>>> task = ConvertXFM(input_matrix="AtoB.mat", concat_matrix="BtoC.mat", inverse=True, output_matrix="CtoA.mat")
+>>> task.cmdline
+'convert_xfm -omat CtoA.mat -concat BtoC.mat -inverse AtoB.mat'
 """
 
-__all__ = ["ConvertXFM", "BaseConvertXFMSpec"]
+__all__ = ["ConvertXFM", "ConcatXFM", "InvertXFM", "FixScaleSkew"]
 
 import os
 
@@ -28,7 +40,7 @@ import pydra
 
 
 @attrs.define(slots=False, kw_only=True)
-class BaseConvertXFMSpec(pydra.specs.ShellSpec):
+class BaseSpec(pydra.specs.ShellSpec):
     """Base specifications for all tasks using convert_xfm."""
 
     input_matrix: os.PathLike = attrs.field(
@@ -50,22 +62,18 @@ class BaseConvertXFMSpec(pydra.specs.ShellSpec):
 
 
 @attrs.define(slots=False, kw_only=True)
-class ConvertXFMSpec(BaseConvertXFMSpec):
+class ConvertXFMSpec(BaseSpec):
     """Specifications for convert_xfm."""
 
-    concat_matrix: os.PathLike = attrs.field(
-        metadata={
-            "help_string": "concatenate this matrix with input matrix",
-            "argstr": "-concat",
-        }
+    fixscaleskew_matrix: os.PathLike = attrs.field(
+        metadata={"help_string": " fix scaling and skewness with this matrix", "argstr": "-fixscaleskew"}
     )
 
-    inverse: bool = attrs.field(
-        metadata={
-            "help_string": "return inverse of computed matrix",
-            "argstr": "-inverse",
-        }
+    concat_matrix: os.PathLike = attrs.field(
+        metadata={"help_string": "concatenate with this matrix", "argstr": "-concat"}
     )
+
+    inverse: bool = attrs.field(metadata={"help_string": "invert the resulting matrix", "argstr": "-inverse"})
 
 
 class ConvertXFM(pydra.engine.ShellCommandTask):
@@ -73,4 +81,51 @@ class ConvertXFM(pydra.engine.ShellCommandTask):
 
     executable = "convert_xfm"
 
-    input_spec = pydra.specs.SpecInfo(name="ConvertXFMInput", bases=(ConvertXFMSpec,))
+    input_spec = pydra.specs.SpecInfo(name="Inputs", bases=(ConvertXFMSpec,))
+
+
+@attrs.define(slots=False, kw_only=True)
+class ConcatXFMSpec(BaseSpec):
+    """Specifications for concat_xfm."""
+
+    concat_matrix: os.PathLike = attrs.field(
+        metadata={"help_string": "concatenate with this matrix", "mandatory": True, "argstr": "-concat"}
+    )
+
+
+class ConcatXFM(ConvertXFM):
+    """Task definition for matrix concatenation using convert_xfm."""
+
+    input_spec = pydra.specs.SpecInfo(name="Inputs", bases=(ConcatXFMSpec,))
+
+
+@attrs.define(slots=False, kw_only=True)
+class InvertXFMSpec(BaseSpec):
+    """Specifications for invert_xfm."""
+
+    inverse: bool = attrs.field(default=True, metadata={"help_string": "invert the input matrix", "argstr": "-inverse"})
+
+
+class InvertXFM(ConvertXFM):
+    """Task definition for matrix inversion using convert_xfm."""
+
+    input_spec = pydra.specs.SpecInfo(name="Inputs", bases=(InvertXFMSpec,))
+
+
+@attrs.define(slots=False, kw_only=True)
+class FixScaleSkewSpec(BaseSpec):
+    """Specifications for fixing matrix scaling and skewness using convert_xfm."""
+
+    fixscaleskew_matrix: os.PathLike = attrs.field(
+        metadata={
+            "help_string": " fix scaling and skewness with this matrix",
+            "mandatory": True,
+            "argstr": "-fixscaleskew",
+        }
+    )
+
+
+class FixScaleSkew(ConvertXFM):
+    """Task definition for fixing matrix scaling and skewness using convert_xfm."""
+
+    input_spec = pydra.specs.SpecInfo(name="Inputs", bases=(FixScaleSkewSpec,))
