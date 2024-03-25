bebe20a0b3a9b113e7b9873856bcbe451f8af74b
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:23:21 +1100

    NEW: Add task definition for fslreorient2std

---
+++ pydra/tasks/fsl/fslreorient2std.py
@@ -0,0 +1,63 @@
+"""
+fslreorient2std
+===============
+
+Change orientation of the image to match the one used
+for standard template images (MNI152).
+
+Examples
+--------
+
+>>> task = FSLReorient2Std(input_image="image.nii")
+>>> task.cmdline  # doctest: +ELLIPSIS
+'fslreorient2std -m ...image_r2std.mat image.nii ...image_r2std.nii'
+"""
+import os
+
+import attrs
+
+import pydra
+
+__all__ = ["FSLReorient2Std"]
+
+
+@attrs.define(slots=False, kw_only=True)
+class FSLReorient2StdSpec(pydra.specs.ShellSpec):
+    """Specificiations for fslreorient2std."""
+
+    input_image: os.PathLike = attrs.field(
+        metadata={
+            "help_string": "input image",
+            "mandatory": True,
+            "argstr": "",
+            "position": -2,
+        }
+    )
+
+    output_image: str = attrs.field(
+        metadata={
+            "help_string": "output reoriented image",
+            "argstr": "",
+            "position": -1,
+            "output_file_template": "{input_image}_r2std",
+        }
+    )
+
+    output_matrix: str = attrs.field(
+        metadata={
+            "help_string": "output transformation matrix",
+            "argstr": "-m",
+            "output_file_template": "{input_image}_r2std.mat",
+            "keep_extension": False,
+        }
+    )
+
+
+class FSLReorient2Std(pydra.engine.ShellCommandTask):
+    """Task definition for fslreorient2std."""
+
+    input_spec = pydra.specs.SpecInfo(
+        name="FSLReorient2StdInput", bases=(FSLReorient2StdSpec,)
+    )
+
+    executable = "fslreorient2std"
