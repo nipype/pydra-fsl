aec9c6d4fa4569b66e9079a329c712a584485011
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:28:52 +1100

    ENH: Add templated output parameters

--- pydra/tasks/fsl/fnirt.py
+++ pydra/tasks/fsl/fnirt.py
@@ -38,6 +38,31 @@ class FNIRTSpec(pydra.specs.ShellSpec):
         metadata={"help_string": "input image", "argstr": "--in"}
     )
 
+    output_field_coefficients_file: str = attrs.field(
+        metadata={
+            "help_string": "output file containing the field coefficients",
+            "argstr": "--cout",
+            "output_file_template": "{input_image}_fnirt_coeff",
+            "keep_extension": False,
+        }
+    )
+
+    output_image: str = attrs.field(
+        metadata={
+            "help_string": "output image",
+            "argstr": "--iout",
+            "output_file_template": "{input_image}_fnirt",
+        }
+    )
+
+    output_field_image: str = attrs.field(
+        metadata={
+            "help_string": "output deformation field",
+            "argstr": "--fout",
+            "output_file_template": "{input_image}_fnirt_field",
+        }
+    )
+
     verbose: bool = attrs.field(
         metadata={
             "help_string": "enable verbose logging",
