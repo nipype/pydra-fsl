dee9f268e776df3caeb5a9f60547fa0239b66cfb
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:49:31 +1100

    ENH: Major improvement for FSLROI
    - Use upstream naming convention for fields (min_x -> x_min ...)
    - Require specification of ROI location in x, y and z together
    - Default t_min to 0 and t_max to -1

--- src/pydra/tasks/fsl/utils/fslroi.py
+++ src/pydra/tasks/fsl/utils/fslroi.py
@@ -10,128 +10,77 @@ Examples
 Extract a 16-voxel cube starting at position (10, 20, 30):
 
 >>> task = FSLROI(
-...    input_image="image.nii",
-...    min_x=10,
-...    min_y=20,
-...    min_z=30,
-...    size_x=16,
-...    size_y=16,
-...    size_z=16,
+...     input_image="image.nii",
+...     x_min=10,
+...     x_size=16,
+...     y_min=20,
+...     y_size=16,
+...     z_min=30,
+...     z_size=16,
 ... )
->>> task.cmdline  # doctest: +ELLIPSIS
-'fslroi image.nii ...image_roi.nii 10 16 20 16 30 16'
+>>> task.cmdline    # doctest: +ELLIPSIS
+'fslroi image.nii ...image_roi.nii 10 16 20 16 30 16 ...'
 
 Extract a temporal window starting at 5 onwards:
 
->>> task = FSLROI(
-...     input_image="input.nii",
-...     output_image="output.nii",
-...     min_t=5,
-...     size_t=-1,
-... )
+>>> task = FSLROI(input_image="input.nii", output_image="output.nii", t_min=5)
 >>> task.cmdline
 'fslroi input.nii output.nii 5 -1'
 """
 
 __all__ = ["FSLROI"]
 
-import os
+from os import PathLike
 
-import attrs
+from attrs import define, field
+from pydra.engine.specs import ShellSpec, SpecInfo
+from pydra.engine.task import ShellCommandTask
 
-import pydra
 
-
-@attrs.define(slots=False, kw_only=True)
-class FSLROISpec(pydra.specs.ShellSpec):
+@define(kw_only=True)
+class FSLROISpec(ShellSpec):
     """Specifications for fslroi."""
 
-    input_image: os.PathLike = attrs.field(
-        metadata={
-            "help_string": "input image",
-            "mandatory": True,
-            "argstr": "",
-        }
+    _requires = {"x_min", "y_min", "z_min"}
+
+    input_image: PathLike = field(metadata={"help_string": "input image", "mandatory": True, "argstr": ""})
+
+    output_image: str = field(
+        metadata={"help_string": "output image", "argstr": "", "output_file_template": "{input_image}_roi"}
     )
 
-    output_image: str = attrs.field(
-        metadata={
-            "help_string": "output image",
-            "argstr": "",
-            "output_file_template": "{input_image}_roi",
-        }
+    x_min: int = field(
+        metadata={"help_string": "start of ROI in x (0-based indexing)", "argstr": "", "requires": _requires}
     )
 
-    min_x: int = attrs.field(
-        metadata={
-            "help_string": "start of ROI in x-dimension (0-based indexing)",
-            "argstr": "",
-        }
+    x_size: int = field(
+        metadata={"help_string": "size of ROI in x (-1 for maximum)", "argstr": "", "requires": {"x_min"}}
     )
 
-    size_x: int = attrs.field(
-        # default=-1,
-        metadata={
-            "help_string": "size of ROI in x-dimension (-1 for maximum)",
-            "argstr": "",
-            "requires": {"min_x"},
-        }
+    y_min: int = field(
+        metadata={"help_string": "start of ROI in y (0-based indexing)", "argstr": "", "requires": _requires}
     )
 
-    min_y: int = attrs.field(
-        metadata={
-            "help_string": "start of ROI in y-dimension (0-based indexing)",
-            "argstr": "",
-            "requires": {"min_x"},
-        }
+    y_size: int = field(
+        metadata={"help_string": "size of ROI in y (-1 for maximum)", "argstr": "", "requires": {"y_min"}}
     )
 
-    size_y: int = attrs.field(
-        # default=-1,
-        metadata={
-            "help_string": "size of ROI in y-dimension (-1 for maximum)",
-            "argstr": "",
-            "requires": {"min_y"},
-        }
+    z_min: int = field(
+        metadata={"help_string": "start of ROI in z (0-based indexing)", "argstr": "", "requires": _requires}
     )
 
-    min_z: int = attrs.field(
-        metadata={
-            "help_string": "start of ROI in z-dimension (0-based indexing)",
-            "argstr": "",
-            "requires": {"min_x"},
-        }
+    z_size: int = field(
+        metadata={"help_string": "size of ROI in z (-1 for maximum)", "argstr": "", "requires": {"z_min"}}
     )
 
-    size_z: int = attrs.field(
-        # default=-1,
-        metadata={
-            "help_string": "size of ROI in z-dimension (-1 for maximum)",
-            "argstr": "",
-            "requires": {"min_z"},
-        }
-    )
+    t_min: int = field(default=0, metadata={"help_string": "start of ROI in t (0-based indexing)", "argstr": ""})
 
-    min_t: int = attrs.field(
-        metadata={
-            "help_string": "start of ROI in t-dimension (0-based indexing)",
-            "argstr": "",
-        }
-    )
-
-    size_t: int = attrs.field(
-        # default=-1,
-        metadata={
-            "help_string": "size of ROI in t-dimension (-1 for maximum)",
-            "argstr": "",
-            "requires": {"min_t"},
-        },
-    )
+    t_size: int = field(default=-1, metadata={"help_string": "size of ROI in t (-1 for maximum)", "argstr": ""})
 
 
-class FSLROI(pydra.engine.ShellCommandTask):
+class FSLROI(ShellCommandTask):
     """Task definition for fslroi."""
 
     executable = "fslroi"
 
-    input_spec = pydra.engine.specs.SpecInfo(name="FSLROIInput", bases=(FSLROISpec,))
+    input_spec = SpecInfo(name="Inputs", bases=(FSLROISpec,))
