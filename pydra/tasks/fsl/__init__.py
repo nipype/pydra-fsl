2aa203c5597cf2c253be73ae151103a08aae4e9c
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:30:19 +1100

    REF: Move FLIRT interface and utilities to their own subpackage

--- pydra/tasks/fsl/__init__.py
+++ pydra/tasks/fsl/__init__.py
@@ -7,32 +7,26 @@ imported.
 """
 
 __all__ = [
-    "ApplyXFM",
     "BET",
-    "ConcatXFM",
-    "ConvertXFM",
+    "Eddy",
     "FAST",
-    "FLIRT",
     "FNIRT",
     "FSLMerge",
     "FSLReorient2Std",
     "FSLROI",
-    "InvertXFM",
     "RobustFOV",
+    "flirt",
     "fslmaths",
 ]
 
-from . import fslmaths
-from .apply_xfm import ApplyXFM
+from . import flirt, fslmaths
 from .bet import BET
-from .concat_xfm import ConcatXFM
-from .convert_xfm import ConvertXFM
 from .eddy import Eddy
 from .fast import FAST
-from .flirt import FLIRT
 from .fnirt import FNIRT
 from .fslmerge import FSLMerge
 from .fslreorient2std import FSLReorient2Std
 from .fslroi import FSLROI
-from .invert_xfm import InvertXFM
 from .robustfov import RobustFOV
+
+__all__ += flirt.__all__
---
+++ pydra/tasks/fsl/flirt/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "FLIRT",
+    "ApplyXFM",
+    "ConcatXFM",
+    "ConvertXFM",
+    "InvertXFM",
+]
+
+from .apply_xfm import ApplyXFM
+from .concat_xfm import ConcatXFM
+from .convert_xfm import ConvertXFM
+from .flirt import FLIRT
+from .invert_xfm import InvertXFM
