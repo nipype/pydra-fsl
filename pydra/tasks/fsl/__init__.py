fd5dcbd1b34741995d337c6257a66f945deed6cd
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:31:17 +1100

    NEW: Add task definition for fnirtfileutils

--- pydra/tasks/fsl/__init__.py
+++ pydra/tasks/fsl/__init__.py
@@ -10,7 +10,7 @@ from .bet import BET, RobustFOV
 from .eddy import Eddy
 from .fast import FAST
 from .flirt import FLIRT, ApplyXFM, ConcatXFM, ConvertXFM, InvertXFM
-from .fnirt import FNIRT, ApplyWarp, ConvertWarp, InvWarp
+from .fnirt import FNIRT, ApplyWarp, ConvertWarp, FNIRTFileUtils, InvWarp
 from .fugue import FUGUE
 from .susan import SUSAN
 from .utils import (
--- pydra/tasks/fsl/fnirt/__init__.py
+++ pydra/tasks/fsl/fnirt/__init__.py
@@ -1,4 +1,5 @@
 from .applywarp import ApplyWarp
 from .convertwarp import ConvertWarp
 from .fnirt import FNIRT
+from .fnirtfileutils import FNIRTFileUtils
 from .invwarp import InvWarp
