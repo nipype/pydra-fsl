f4afad7f82198b1fa5b91c42f37b20f559bfe9d1
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2024-03-25 12:24:51 +1100

    NEW: Add task definition for FAST

--- pydra/tasks/fsl/__init__.py
+++ pydra/tasks/fsl/__init__.py
@@ -6,33 +6,24 @@ imported.
 >>> import pydra.tasks.fsl
 """
 
-from warnings import warn
-from pathlib import Path
+from .bet import BET
+from .convert_xfm import ConvertXFM
+from .fast import FAST
+from .flirt import FLIRT
+from .fnirt import FNIRT
+from .fslmerge import FSLMerge
+from .fslreorient2std import FSLReorient2Std
+from .fslroi import FSLROI
+from .robustfov import RobustFOV
 
-pkg_path = Path(__file__).parent.parent
-
-try:
-    from ._version import __version__
-except ImportError:
-    raise RuntimeError(
-        "pydra-fsl has not been properly installed, please run "
-        f"`pip install -e {str(pkg_path)}` to install a development version"
-    )
-if "nipype" not in __version__:
-    try:
-        from .auto._version import nipype_version, nipype2pydra_version
-    except ImportError:
-        warn(
-            "Nipype interfaces haven't been automatically converted from their specs in "
-            f"`nipype-auto-conv`. Please run `{str(pkg_path / 'nipype-auto-conv' / 'generate')}` "
-            "to generated the converted Nipype interfaces in pydra.tasks.fsl.auto"
-        )
-    else:
-        n_ver = nipype_version.replace(".", "_")
-        n2p_ver = nipype2pydra_version.replace(".", "_")
-        __version__ += (
-            "_" if "+" in __version__ else "+"
-        ) + f"nipype{n_ver}_nipype2pydra{n2p_ver}"
-
-
-__all__ = ["__version__"]
+__all__ = [
+    "BET",
+    "ConvertXFM",
+    "FAST",
+    "FLIRT",
+    "FNIRT",
+    "FSLMerge",
+    "FSLReorient2Std",
+    "FSLROI",
+    "RobustFOV",
+]
---
+++ pydra/tasks/fsl/fast.py
@@ -0,0 +1,202 @@
+"""
+FAST
+====
+"""
+import os
+import typing as ty
+
+import attrs
+
+import pydra
+
+__all__ = ["FAST"]
+
+
+@attrs.define(slots=False, kw_only=True)
+class FASTSpec(pydra.specs.ShellSpec):
+    """Specifications for FAST."""
+
+    # Input parameters.
+    input_image: os.PathLike = attrs.field(
+        metadata={
+            "help_string": "input image (single-channel mode)",
+            "mandatory": True,
+            "argstr": "",
+            "position": -1,
+        }
+    )
+
+    image_type: int = attrs.field(
+        default=1,
+        metadata={
+            "help_string": "type of input image(s) (1: T1, 2: T2, 3: PD)",
+            "argstr": "-t",
+            "allowed_values": {1, 2, 3},
+        },
+    )
+
+    # Output parameters.
+    output_basename: str = attrs.field(
+        default="fast",
+        metadata={
+            "help_string": "basename used for output files",
+            "argstr": "-o",
+        },
+    )
+
+    num_classes: int = attrs.field(
+        default=3,
+        metadata={
+            "help_string": "number of tissue-type classes",
+            "argstr": "-n",
+        },
+    )
+
+    save_probability_maps: bool = attrs.field(
+        metadata={
+            "help_string": "save probability map for each class",
+            "argstr": "-p",
+        }
+    )
+
+    save_bias_field_image: bool = attrs.field(
+        metadata={
+            "help_string": "save estimated bias field",
+            "argstr": "-b",
+        }
+    )
+
+    save_bias_corrected_image: bool = attrs.field(
+        metadata={
+            "help_string": "save restored image after bias field correction",
+            "argstr": "-B",
+        }
+    )
+
+    save_segmentation_masks: bool = attrs.field(
+        metadata={
+            "help_string": "save segmentation mask for each class",
+            "argstr": "-g",
+        }
+    )
+
+    # Advanced parameters.
+    main_mrf_parameter: float = attrs.field(
+        default=0.1,
+        metadata={
+            "help_string": "",
+            "argstr": "-H",
+        },
+    )
+
+    bias_field_iterations: int = attrs.field(
+        default=4,
+        metadata={
+            "help_string": "number of iterations for bias field removal",
+            "argstr": "-I",
+        },
+    )
+
+    bias_field_smoothing: float = attrs.field(
+        default=20,
+        metadata={
+            "help_string": "bias field smoothing (FWHM in millimeters)",
+            "argstr": "-l",
+        },
+    )
+
+    no_partial_volume_estimation: bool = attrs.field(
+        metadata={
+            "help_string": "do not perform partial volume estimation",
+            "argstr": "--nopve",
+        }
+    )
+
+
+class FASTOutSpec(pydra.specs.ShellOutSpec):
+    """Ouput specifications for FAST."""
+
+    @staticmethod
+    def get_segmentation_image(output_basename: str) -> str:
+        return f"{output_basename}_seg"
+
+    @staticmethod
+    def get_segmentation_masks(output_basename: str, num_classes: int) -> ty.List[str]:
+        return [f"{output_basename}_seg_{i}" for i in range(num_classes)]
+
+    @staticmethod
+    def get_probability_maps(output_basename: str, num_classes: int) -> ty.List[str]:
+        return [f"{output_basename}_prob_{i}" for i in range(num_classes)]
+
+    @staticmethod
+    def get_partial_volume_maps(output_basename: str, num_classes: int) -> ty.List[str]:
+        return [f"{output_basename}_pve_{i}" for i in range(num_classes)]
+
+    @staticmethod
+    def get_bias_field_image(output_basename: str) -> str:
+        return f"{output_basename}_bias"
+
+    @staticmethod
+    def get_bias_corrected_image(output_basename: str) -> str:
+        return f"{output_basename}_restore"
+
+    segmentation_image: str = attrs.field(
+        metadata={
+            "help_string": "segmentation image with each voxel assigned a class",
+            "mandatory": True,
+            "callable": get_segmentation_image,
+        }
+    )
+
+    segmentation_masks: ty.List[str] = attrs.field(
+        metadata={
+            "help_string": (
+                "one segmentation mask per class, each voxel is assigned a value of "
+                "1 if belonging to the class 0 otherwise."
+            ),
+            "requires": {"save_segmentation_masks"},
+            "callable": get_segmentation_masks,
+        }
+    )
+
+    probability_maps: ty.List[str] = attrs.field(
+        metadata={
+            "help_string": "posterior probablity mapping for each class",
+            "requires": {"save_probability_maps"},
+            "callable": get_probability_maps,
+        }
+    )
+
+    partial_volume_maps: ty.List[str] = attrs.field(
+        metadata={
+            "help_string": "partial volume mapping for each class",
+            "requires": {("no_partial_volume_estimation", False)},
+            "callable": get_partial_volume_maps,
+        }
+    )
+
+    bias_field_image: str = attrs.field(
+        metadata={
+            "help_string": "estimated bias field",
+            "requires": {"save_bias_field_image"},
+            "callable": get_bias_field_image,
+        }
+    )
+
+    bias_corrected_image: str = attrs.field(
+        metadata={
+            "help_string": "restored input image after bias field correction",
+            "requires": {"save_bias_corrected_image"},
+            "callable": get_bias_corrected_image,
+        }
+    )
+
+
+class FAST(pydra.engine.ShellCommandTask):
+    """Task definition for FAST."""
+
+    input_spec = pydra.specs.SpecInfo(name="FASTInput", bases=(FASTSpec,))
+
+    output_spec = pydra.specs.SpecInfo(name="FASTOuput", bases=(FASTOutSpec,))
+
+    executable = "fast"
