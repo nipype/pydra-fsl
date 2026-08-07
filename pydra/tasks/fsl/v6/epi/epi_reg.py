decd696e03affd5ae303f1ccc8fd0d585cf1fa73
Thomas G. Close <tom.g.close@gmail.com>
2026-08-07 16:15:42 +1000

    reverted type of epi_reg to Nifti from File

--- pydra/tasks/fsl/v6/epi/epi_reg.py
+++ pydra/tasks/fsl/v6/epi/epi_reg.py
@@ -1,11 +1,10 @@
 import attrs
 from fileformats.generic import File
-from fileformats.medimage import Nifti1
+from fileformats.medimage import Nifti, Nifti1, NiftiGz
 import logging
 import os
 from pydra.compose import shell
 import typing as ty
-
 
 logger = logging.getLogger(__name__)
 
@@ -177,11 +176,11 @@ class EpiReg(shell.Task["EpiReg.Outputs"]):
     """
 
     executable = "epi_reg"
-    epi: File = shell.arg(help="EPI image", argstr="--epi={epi}", position=-4)
-    t1_head: File = shell.arg(
+    epi: Nifti = shell.arg(help="EPI image", argstr="--epi={epi}", position=-4)
+    t1_head: Nifti = shell.arg(
         help="wholehead T1 image", argstr="--t1={t1_head}", position=-3
     )
-    t1_brain: File = shell.arg(
+    t1_brain: Nifti = shell.arg(
         help="brain extracted T1 image", argstr="--t1brain={t1_brain}", position=-2
     )
     out_base: ty.Any = shell.arg(
@@ -190,7 +189,7 @@ class EpiReg(shell.Task["EpiReg.Outputs"]):
         position=-1,
         default="epi2struct",
     )
-    fmap: Nifti1 | None = shell.arg(
+    fmap: Nifti | None = shell.arg(
         help="fieldmap image (in rad/s)", argstr="--fmap={fmap}", default=None
     )
     fmapmag: File | None = shell.arg(
@@ -198,12 +197,12 @@ class EpiReg(shell.Task["EpiReg.Outputs"]):
         argstr="--fmapmag={fmapmag}",
         default=None,
     )
-    fmapmagbrain: Nifti1 | None = shell.arg(
+    fmapmagbrain: Nifti | None = shell.arg(
         help="fieldmap magnitude image - brain extracted",
         argstr="--fmapmagbrain={fmapmagbrain}",
         default=None,
     )
-    wmseg: File | None = shell.arg(
+    wmseg: Nifti | None = shell.arg(
         help="white matter segmentation of T1 image, has to be named                  like the t1brain and end on _wmseg",
         argstr="--wmseg={wmseg}",
         default=None,
@@ -218,7 +217,7 @@ class EpiReg(shell.Task["EpiReg.Outputs"]):
         argstr="--pedir={pedir}",
         default=None,
     )
-    weight_image: File | None = shell.arg(
+    weight_image: Nifti | None = shell.arg(
         help="weighting image (in T1 space)",
         argstr="--weight={weight_image}",
         default=None,
@@ -233,48 +232,48 @@ class EpiReg(shell.Task["EpiReg.Outputs"]):
     )
 
     class Outputs(shell.Outputs):
-        out_file: File | None = shell.out(
+        out_file: NiftiGz | None = shell.out(
             help="unwarped and coregistered epi input", callable=out_file_callable
         )
-        out_1vol: File | None = shell.out(
+        out_1vol: NiftiGz | None = shell.out(
             help="unwarped and coregistered single volume", callable=out_1vol_callable
         )
-        fmap2str_mat: File | None = shell.out(
+        fmap2str_mat: NiftiGz | None = shell.out(
             help="rigid fieldmap-to-structural transform",
             callable=fmap2str_mat_callable,
         )
-        fmap2epi_mat: File | None = shell.out(
+        fmap2epi_mat: NiftiGz | None = shell.out(
             help="rigid fieldmap-to-epi transform", callable=fmap2epi_mat_callable
         )
-        fmap_epi: File | None = shell.out(
+        fmap_epi: NiftiGz | None = shell.out(
             help="fieldmap in epi space", callable=fmap_epi_callable
         )
-        fmap_str: File | None = shell.out(
+        fmap_str: NiftiGz | None = shell.out(
             help="fieldmap in structural space", callable=fmap_str_callable
         )
-        fmapmag_str: File | None = shell.out(
+        fmapmag_str: NiftiGz | None = shell.out(
             help="fieldmap magnitude image in structural space",
             callable=fmapmag_str_callable,
         )
-        epi2str_inv: File | None = shell.out(
+        epi2str_inv: NiftiGz | None = shell.out(
             help="rigid structural-to-epi transform", callable=epi2str_inv_callable
         )
-        epi2str_mat: File | None = shell.out(
+        epi2str_mat: NiftiGz | None = shell.out(
             help="rigid epi-to-structural transform", callable=epi2str_mat_callable
         )
-        shiftmap: File | None = shell.out(
+        shiftmap: NiftiGz | None = shell.out(
             help="shiftmap in epi space", callable=shiftmap_callable
         )
-        fullwarp: File | None = shell.out(
+        fullwarp: NiftiGz | None = shell.out(
             help="warpfield to unwarp epi and transform into                     structural space",
             callable=fullwarp_callable,
         )
-        wmseg: File | None = shell.out(
+        wmseg: NiftiGz | None = shell.out(
             help="white matter segmentation used in flirt bbr", callable=wmseg_callable
         )
-        seg: File | None = shell.out(
+        seg: NiftiGz | None = shell.out(
             help="white matter, gray matter, csf segmentation", callable=seg_callable
         )
-        wmedge: File | None = shell.out(
+        wmedge: NiftiGz | None = shell.out(
             help="white matter edges for visualization", callable=wmedge_callable
         )
